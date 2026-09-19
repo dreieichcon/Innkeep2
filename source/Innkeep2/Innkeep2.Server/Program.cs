669850d580b166441cdf75fd2169ba8d4c28ce34
Tobias Brinke <tobi.93+git@pm.me>
2026-09-19 13:46:44 +0200

    Server Basic Setup

--- source/Innkeep2/Innkeep2.Server/Program.cs
+++ source/Innkeep2/Innkeep2.Server/Program.cs
@@ -1,10 +1,13 @@
 using Innkeep2.Server.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
@@ -27,4 +30,4 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode();
 
-app.Run();
+await app.RunAsync();
